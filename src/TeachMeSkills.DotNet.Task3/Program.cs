e427b4c5d957f864b42a503e1fd605e20975ac37
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2020-09-18 22:17:49 +0300

    feat: add solution Task 3

---
+++ src/TeachMeSkills.DotNet.Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = Math.PI;
+            Console.Write("Enter radius : ");
+            double r = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Square = {0:00.00}", (double)(pi * r * r));
+        }
+    }
+}
