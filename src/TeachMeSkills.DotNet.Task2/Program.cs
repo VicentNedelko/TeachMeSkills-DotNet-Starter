caca19a02d91dcf1dd90e9fb40f35d58bbb83457
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2020-09-18 21:22:41 +0300

    feat: finish solution Task 2

---
+++ src/TeachMeSkills.DotNet.Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "\nмоя строка 1";
+            string str2 = "\tмоя строка 2";
+            string str3 = "\aмоя строка 3";
+
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+            Console.WriteLine(str3);
+        }
+    }
+}
