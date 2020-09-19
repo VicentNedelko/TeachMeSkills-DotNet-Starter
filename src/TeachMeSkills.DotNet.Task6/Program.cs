e806618c718472bc5859b6a0d60f3a0ba3cc20f9
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2020-09-19 14:04:20 +0300

    feat: add solution Task 6

---
+++ src/TeachMeSkills.DotNet.Task6/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter operand1 : ");
+            double operand1 = Double.Parse(Console.ReadLine());
+            Console.Write("Enter operand2 : ");
+            double operand2 = Double.Parse(Console.ReadLine());
+            Console.Write("Enter operation : ");
+            string operation = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("--------------------------------------");
+            Console.ForegroundColor = ConsoleColor.White;
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine("Result : {0:0.00} {1} {2:0.00} = {3:0.00}", operand1, operation, operand2, operand1 + operand2);
+                    break;
+                case "-":
+                    Console.WriteLine("Result : {0:0.00} {1} {2:0.00} = {3:0.00}", operand1, operation, operand2, operand1 - operand2);
+                    break;
+                case "*":
+                    Console.WriteLine("Result : {0:0.00} {1} {2:0.00} = {3:0.00}", operand1, operation, operand2, operand1 * operand2);
+                    break;
+                case "/":
+                    if (operand2 != 0) Console.WriteLine("Result : {0:0.00} {1} {2:0.00} = {3:0.00}", operand1, operation, operand2, operand1 / operand2);
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Error! Devision by zero.");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    break;
+                default:
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Error! Wrong operation.");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    break;
+            }
+        }
+    }
+}
