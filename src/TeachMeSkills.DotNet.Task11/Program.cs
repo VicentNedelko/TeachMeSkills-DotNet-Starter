b14bf75c69e5487defc179ef62c4f3d3fcf33f6e
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2020-09-19 17:57:20 +0300

    feat: add solution of Task 11

---
+++ src/TeachMeSkills.DotNet.Task11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter value : ");
+            int value = Int32.Parse(Console.ReadLine());
+            int rest = 0;
+            while (value > 1 && rest == 0)
+            {
+                rest = value % 2;
+                value /= 2;
+            }
+            if (rest != 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Value is NOT a power of 2");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Value is a power of 2");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}
