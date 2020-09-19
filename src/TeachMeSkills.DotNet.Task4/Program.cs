9a85804e90c0f8dad92a74f6bc8f9363943eeec6
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2020-09-19 13:11:39 +0300

    feat: add solution Task 4

---
+++ src/TeachMeSkills.DotNet.Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Radius : ");
+            double radius = Double.Parse(Console.ReadLine());
+            Console.Write("Enter Height : ");
+            double height = Double.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("---------------------------------------");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Volume : {0:000.00}", Math.PI * radius * radius * height);
+            Console.WriteLine("Square : {0:000.00}", 2 * Math.PI * radius * (radius + height));
+        }
+    }
+}
