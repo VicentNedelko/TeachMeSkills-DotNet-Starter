d47bea0a29655ee0ca5c77e01d1199d96fbf1483
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2020-09-19 18:48:09 +0300

    feat: add solution Task 13

---
+++ src/TeachMeSkills.DotNet.Task13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter A : ");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter B : ");
+            int b = Int32.Parse(Console.ReadLine());
+            int sum = 0;
+            Console.WriteLine("---------------------------------");
+            for (int i = a + 1; i < b; i++)
+            {
+                sum += i;
+                if (i % 2 == 1)
+                {
+                    Console.Write("{0} ; ", i);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Sum : {0}", sum);
+        }
+    }
+}
