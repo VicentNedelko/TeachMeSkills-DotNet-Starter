585bf61ff7ac98ca903909457484df2c937e5736
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2020-09-19 14:22:53 +0300

    feat: add solution Task 7

---
+++ src/TeachMeSkills.DotNet.Task7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter digit : ");
+            int digit = Int32.Parse(Console.ReadLine());
+            if (digit >= 0 && digit <= 14) Console.WriteLine("Range : [0 - 14]");
+            else if (digit >= 15 && digit <= 35)
+                Console.WriteLine("Range : [15 - 35]");
+            else if (digit >= 36 && digit <= 50)
+                Console.WriteLine("Range : [36 - 50]");
+            else if (digit >= 51 && digit <= 100)
+                Console.WriteLine("Range : [51 - 100]");
+            else Console.WriteLine("Digit is out all ranges.");
+        }
+    }
+}
