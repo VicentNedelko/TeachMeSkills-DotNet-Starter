b0067968d6e4686e24acddfd0b1eed2572147022
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2020-09-24 18:45:45 +0300

    feat: add solution for Task 19

---
+++ src/TeachMeSkills.DotNet.Task19/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task19
+{
+    class Program
+    {
+        static void GetNumbers(int startDigit, int endDigit)
+        {
+            for (int i = startDigit; i <= endDigit; i++)
+            {
+                int devSum = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0) { devSum++; }
+                }
+                if (devSum > 2)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("{0} is a composite number.", i);
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else if (devSum == 2 || devSum == 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("{0} is a simple number.", i);
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Start number : ");
+            int start = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter End number : ");
+            int end = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("-----------------------------");
+            GetNumbers(start, end);
+        }
+    }
+}
