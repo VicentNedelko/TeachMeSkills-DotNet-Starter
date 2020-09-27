4fd434661507b0232476aa8211c1303008ecf6e3
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2020-09-27 17:05:10 +0300

    feat: add solution for Task12

---
+++ src/TeachMeSkills.DetNet.Task12/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TeachMeSkills.DetNet.Task12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A | B = (A | B)->!(!A & !B)");
+            Console.WriteLine("---------------------");
+            bool A = false;
+            bool B = false;
+            Console.WriteLine("0 | 0 = {0} -> {1}", A || B, !(!A && !B));
+            B = true;
+            Console.WriteLine("0 | 1 = {0} -> {1}", A || B, !(!A && !B));
+            A = true;
+            B = false;
+            Console.WriteLine("1 | 0 = {0} -> {1}", A || B, !(!A && !B));
+            B = true;
+            Console.WriteLine("1 | 1 = {0} -> {1}", A || B, !(!A && !B));
+        }
+    }
+}
